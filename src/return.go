6408e19579fdfe6b3f1aea49c4b7f9b50fda80d5
skatsuta <katsuta_soshi@cyberagent.co.jp>
2015-01-02 21:53:54 +0900

    [add] add return.go

---
+++ src/return.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	add, sub, mul, div := calc(1, 2)
+
+	fmt.Println(add, sub, mul, div)
+}
+
+func calc(a int, b int) (add int, sub int, mul int, div float32) {
+	add = a + b
+	sub = a - b
+	mul = a * b
+	div = float32(a) / float32(b)
+
+	return
+}
